b260d5ee346e07d54011fb29258cc14e0487a2d0
0x00byte <0x00byte@6c6f6c.net>
2015-02-27 23:32:09 +0100

    Added copyright notice

--- classes/Xo_Init.php
+++ classes/Xo_Init.php
@@ -1,4 +1,22 @@
 <?php
+/*************************************************************************
+ *
+ * EVERETI CONFIDENTIAL
+ * __________________
+ *
+ *  [2013] - [2015] Evereti Ltd
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Evereti Ltd and its suppliers, if any.
+ * The intellectual and technical concepts contained
+ * herein are proprietary to Evereti Ltd
+ * and its suppliers and may be covered by U.S, European and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Evereti Ltd.
+ */
 
 	// INITIALIZES CLASSES AND DBC VARS
 	abstract class Xo_Init extends Xo_Security {
--- classes/Xo_Video.php
+++ classes/Xo_Video.php
@@ -1,5 +1,24 @@
 <?php
 
+/*************************************************************************
+ *
+ * EVERETI CONFIDENTIAL
+ * __________________
+ *
+ *  [2013] - [2015] Evereti Ltd
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Evereti Ltd and its suppliers, if any.
+ * The intellectual and technical concepts contained
+ * herein are proprietary to Evereti Ltd
+ * and its suppliers and may be covered by U.S, European and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Evereti Ltd.
+ */
+
 	// UPLOAD CLASS
 	class Xo_Video extends Xo_Upload {
 
